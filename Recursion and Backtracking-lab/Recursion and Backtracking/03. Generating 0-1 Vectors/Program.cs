9cc0b743222201ee857efc59a441bbeb8fc2c175
Hakan Osman <haki2001@abv.bg>
2024-05-31 09:40:53 +0300

    commit

---
+++ Recursion and Backtracking-lab/Recursion and Backtracking/03. Generating 0-1 Vectors/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+namespace _03._Generating_0_1_Vectors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int n=int.Parse(Console.ReadLine());
+           int[] vec = new int[n];
+            Gen01(vec, 0);
+        }
+
+        private static void Gen01(int[] vec, int idx)
+        {
+           if(idx==vec.Length)
+           {
+              string row=string.Join(string.Empty, vec);
+              Console.WriteLine(row);
+              return;
+
+           }
+            for (int i = 0; i <= 1; i++)
+            {
+                vec[idx] = i;
+                Gen01(vec, idx + 1);
+            }
+        }
+    }
+}
