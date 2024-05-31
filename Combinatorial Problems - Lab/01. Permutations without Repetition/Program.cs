64e6363c8475219376a84eae510fba07e17714cf
Hakan Osman <haki2001@abv.bg>
2024-05-31 19:32:27 +0300

    Pemutions

--- Combinatorial Problems - Lab/01. Permutations without Repetition/Program.cs
+++ Combinatorial Problems - Lab/01. Permutations without Repetition/Program.cs
@@ -3,23 +3,36 @@
     internal class Program
     {
         private static string [] elements;
-        private static string []permutions;
         static void Main(string[] args)
         {
             elements=Console.ReadLine()
            .Split(' ',StringSplitOptions.RemoveEmptyEntries)
            .ToArray();
-            permutions=new string[elements.Length];
             Permute(0);
         }
 
         private static void Permute(int idx)
         {
-            if (idx >= permutions.Length)
+            if (idx >= elements.Length)
             {
-                Console.WriteLine(string.Join(' ',permutions));
+                Console.WriteLine(string.Join(' ',elements));
                 return;
             }
+            Permute(idx + 1);
+            for (int i = idx+1; i < elements.Length; i++)
+            {
+                Swap(idx, i);
+                Permute(idx + 1);
+                Swap(idx, i);
+
+            }
+        }
+
+        private static void Swap(int first, int second)
+        {
+            var temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
         }
     }
 }
