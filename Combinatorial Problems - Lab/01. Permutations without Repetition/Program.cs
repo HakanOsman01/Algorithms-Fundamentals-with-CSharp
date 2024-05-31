403a83668154f9f6813540859e661b5f6a519821
Hakan Osman <haki2001@abv.bg>
2024-05-31 13:46:20 +0300

    Combinations

---
+++ Combinatorial Problems - Lab/01. Permutations without Repetition/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01._Permutations_without_Repetition
+{
+    internal class Program
+    {
+        private static string [] elements;
+        private static string []permutions;
+        static void Main(string[] args)
+        {
+            elements=Console.ReadLine()
+           .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+           .ToArray();
+            permutions=new string[elements.Length];
+            Permute(0);
+        }
+
+        private static void Permute(int idx)
+        {
+            if (idx >= permutions.Length)
+            {
+                Console.WriteLine(string.Join(' ',permutions));
+                return;
+            }
+        }
+    }
+}
