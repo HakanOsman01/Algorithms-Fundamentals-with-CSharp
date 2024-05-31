de05dda962ac53b3060776f250342c26dca7fffc
Hakan Osman <haki2001@abv.bg>
2024-05-31 09:10:42 +0300

    First Exercise

---
+++ Recursion and Backtracking-lab/Recursion and Backtracking/01. Recursive Array Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _01._Recursive_Array_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[]array=Console.ReadLine()
+            .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+            int sum = RecursiveSumArray(array, array.Length - 1);
+            Console.WriteLine(sum);
+        }
+
+        private static int RecursiveSumArray(int[] array, int idx)
+        {
+            if (idx == 0)
+            {
+                return array[idx];
+            }
+            int currentSum = array[idx]+RecursiveSumArray(array, idx-1);
+            return currentSum;
+        }
+    }
+}
