5fb381a7af271a71a022184644694ca4b0d0c213
Hakan Osman <haki2001@abv.bg>
2024-05-31 09:45:18 +0300

    Factoriel

---
+++ Recursion and Backtracking-lab/Recursion and Backtracking/04. Recursive Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Recursive_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num=int.Parse(Console.ReadLine());
+            int fac = CalculateFactorial(num);
+            Console.WriteLine(fac);
+        }
+
+        private static int CalculateFactorial(int num)
+        {
+            if (num == 1)
+            {
+                return 1;
+            }
+            int fac = num * CalculateFactorial(num - 1);
+            return fac;
+
+        }
+    }
+}
