baba35f795b2e25ba8a9c73d0cbf469dac93f4f7
Hakan Osman <haki2001@abv.bg>
2024-06-01 10:00:09 +0300

    Varitions with repetion

--- Combinatorial Problems - Lab/04. Variations with Repetition/Program.cs
+++ Combinatorial Problems - Lab/04. Variations with Repetition/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace _04._Variations_with_Repetition
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace _04._Variations_with_Repetition
 {
     internal class Program
     {
+        private static string[] elements;
+        private static string[] variations;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            elements = Console.ReadLine().Split()
+               .ToArray();
+            int k = int.Parse(Console.ReadLine());
+            variations = new string[k];
+            GenVariations(0);
+
+        }
+        private static void GenVariations(int idx)
+        {
+            if (idx >= variations.Length)
+            {
+                Console.WriteLine(string.Join(' ', variations));
+                return;
+            }
+            for (int i = 0; i < elements.Length; i++)
+            {
+                
+                    variations[idx] = elements[i];
+                    GenVariations(idx + 1);
+            }
         }
     }
 }
---
+++ Combinatorial Problems - Lab/05. Combinations without Repetition/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _05._Combinations_without_Repetition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
