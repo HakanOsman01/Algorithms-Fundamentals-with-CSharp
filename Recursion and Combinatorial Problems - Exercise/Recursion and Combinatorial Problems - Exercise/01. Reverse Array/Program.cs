c3b9f5d722632f1a18b63768531fe8e0568c1f75
Hakan Osman <haki2001@abv.bg>
2024-06-01 10:37:44 +0300

    commit

---
+++ Recursion and Combinatorial Problems - Exercise/Recursion and Combinatorial Problems - Exercise/01. Reverse Array/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Security.Principal;
+
+namespace _01._Reverse_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(num => int.Parse(num))
+            .ToArray();
+            ReverseRecursivelyArray(array, 0, array.Length - 1);
+            PrintRecursive(array, 0);
+        }
+
+        private static void PrintRecursive(int[] array, int idx)
+        {
+           if(idx>=array.Length)
+           {
+                return;
+           }
+           Console.Write($"{array[idx]} ");
+           PrintRecursive(array, idx + 1);
+
+        }
+
+        private static void ReverseRecursivelyArray(int[] array, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            ReverseRecursivelyArray(array,start+1, end-1);
+        }
+    }
+}
