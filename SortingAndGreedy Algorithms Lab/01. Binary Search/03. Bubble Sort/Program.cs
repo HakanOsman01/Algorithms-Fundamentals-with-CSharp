167ad3689bc2b20df8e9a4f1faa84ea3322c2df8
Hakan Osman <haki2001@abv.bg>
2024-06-02 09:32:23 +0300

    Quick Sort

--- SortingAndGreedy Algorithms Lab/01. Binary Search/03. Bubble Sort/Program.cs
+++ SortingAndGreedy Algorithms Lab/01. Binary Search/03. Bubble Sort/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace _03._Bubble_Sort
+﻿using System.Diagnostics.SymbolStore;
+
+namespace _03._Bubble_Sort
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[]elements=Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            BubleSort(elements);
+            Console.WriteLine(string.Join(' ',elements));
+        }
+
+        private static void BubleSort(int[] elements)
+        {
+            bool isSorted=false;
+            var sortedCount = 0;
+            while(!isSorted)
+            {
+                isSorted = true;
+                for (int j = 1; j < elements.Length-sortedCount; j++)
+                {
+                    int i = j - 1;
+                    if (elements[i] > elements[j])
+                    {
+                        Swap(i, j,elements);
+                        isSorted = false;
+                    }
+                }
+                sortedCount++;
+            }
+        }
+        private static void Swap(int first, int second, int[] elements)
+        {
+            var temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
         }
     }
 }
--- SortingAndGreedy Algorithms Lab/01. Binary Search/05. Quicksort/Program.cs
+++ SortingAndGreedy Algorithms Lab/01. Binary Search/05. Quicksort/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace _05._Quicksort
+﻿using System;
+using System.Linq;
+namespace _05._Quicksort
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[]elements=Console.ReadLine().Split(' ')
+                .Select(num=>int.Parse(num))
+                .ToArray();
+            Quicksort(elements,0,elements.Length-1);
+            Console.WriteLine(string.Join(' ',elements));
+        }
+
+        private static void Quicksort(int[] elements, int start, int end)
+        {
+            if(start>=end) 
+            {
+                return;
+            }
+            var pivot = start;
+            var left = start + 1;
+            var right = end;
+            while (left <= right)
+            {
+                if (elements[left] > elements[pivot] 
+                    && elements[right] < elements[pivot])
+                {
+                    Swap(left, right, elements);
+                }
+                if (elements[left] <= elements[pivot])
+                {
+                    left++;
+
+                }
+                if (elements[right] >= elements[pivot])
+                {
+                    right--;
+                }
+
+            }
+            Swap(right,pivot, elements);
+            Quicksort(elements, start, right - 1);
+            Quicksort(elements, right + 1, end);
+        }
+
+        private static void Swap(int first, int second, int[] elements)
+        {
+            var temp = elements[first];
+            elements[first] = elements[second];
+            elements[second] = temp;
         }
     }
 }
